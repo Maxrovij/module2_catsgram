1f62158ba782e44f4a01f1b4227683a6cc9da376
MaximKartsev <karmaig87@yandex.ru>
2022-05-04 12:06:00 +0300

    feature: добавить обработку исключений

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.IncorrectParameterException;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
@@ -22,13 +23,14 @@ public class PostController {
                               @RequestParam(name = "page", defaultValue = "0", required = false) Integer page) {
 
         if (!(sort.equals("desc") || sort.equals("asc"))) {
-            throw new IllegalArgumentException();
+            throw new IncorrectParameterException("sort");
         }
-
-        if(size < 0 || page < 0) {
-            throw new IllegalArgumentException();
+        if(size <= 0) {
+            throw new IncorrectParameterException("size");
         }
-
+        if (page < 0) {
+            throw new IncorrectParameterException("page");
+        }
         Integer from = size * page;
         return postService.findAll(size, sort, from);
     }
--- src/main/java/ru/yandex/practicum/catsgram/controller/PostFeedController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostFeedController.java
@@ -1,15 +1,17 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.catsgram.exception.IncorrectParameterException;
+import ru.yandex.practicum.catsgram.model.FeedParams;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static ru.yandex.practicum.catsgram.Constants.SORTS;
 
 @RestController
 public class PostFeedController {
@@ -21,56 +23,22 @@ public class PostFeedController {
     }
 
     @PostMapping("/feed/friends")
-    List<Post> getFriendsFeed(@RequestBody String request) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        FriendsParams friendsParams;
-        try {
-            String requestValue = objectMapper.readValue(request, String.class);
-            friendsParams = objectMapper.readValue(requestValue, FriendsParams.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Невалидный JSON");
+    List<Post> getFriendsFeed(@RequestBody FeedParams feedParams) {
+        if (!SORTS.contains(feedParams.getSort())) {
+            throw new IncorrectParameterException("sort");
+        }
+        if (feedParams.getFriendsEmails().isEmpty()) {
+            throw new IncorrectParameterException("friendsEmails");
         }
 
-        if (friendsParams != null) {
-            List<Post> result = new ArrayList<>();
-            for (String s : friendsParams.friends) {
-                result.addAll(postService.findAllByEmail(s, friendsParams.size, friendsParams.sort));
-            }
-            return result;
-        } else {
-            throw new RuntimeException("неверные параметры");
-        }
-    }
-
-    static class FriendsParams {
-        private String sort;
-
-        private Integer size;
-
-        private List<String> friends;
-
-        public String getSort() {
-            return sort;
+        if (feedParams.getSize() == null || feedParams.getSize() <= 0) {
+            throw new IncorrectParameterException("size");
         }
 
-        public void setSort(String sort) {
-            this.sort = sort;
+        List<Post> result = new ArrayList<>();
+        for (String friendEmail : feedParams.getFriendsEmails()) {
+            result.addAll(postService.findAllByEmail(friendEmail, feedParams.getSize(), feedParams.getSort()));
         }
-
-        public Integer getSize() {
-            return size;
-        }
-
-        public void setSize(Integer size) {
-            this.size = size;
-        }
-
-        public List<String> getFriends() {
-            return friends;
-        }
-
-        public void setFriends(List<String> friends) {
-            this.friends = friends;
-        }
+        return result;
     }
 }
